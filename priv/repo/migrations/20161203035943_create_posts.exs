538e2d66d4a8fc403e5fa65612396d177c6c5fc3
harrison <harry@lucasconsulting.com.au>
2016-12-03 15:30:09 +1100

    added posts migration

--- priv/repo/migrations/20161203035943_create_posts.exs
+++ priv/repo/migrations/20161203035943_create_posts.exs
@@ -2,6 +2,15 @@ defmodule Blex.Repo.Migrations.CreatePosts do
   use Ecto.Migration
 
   def change do
+    create table(:posts) do
+      add :title, :string
+      add :subtitle, :string
+      add :body, :string
+      add :author, :string
+      add :metadata, :string
+      add :status, :string
 
+      timestamps
+    end
   end
 end
